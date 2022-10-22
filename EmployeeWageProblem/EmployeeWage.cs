e4b0146339032d98591af27ef1100d4f9bfa52ab
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-10-22 09:30:02 +0530

    (Shubham)Add: Employee is Present or Absent

---
+++ EmployeeWageProblem/EmployeeWage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace EmployeeWageProblem
+{
+    public class EmployeeWage
+    {
+        public void Attendance()
+        {
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == 0)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
+        }
+    }
+}
--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace EmployeeWageProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Problem statement");
+            EmployeeWage employee = new EmployeeWage();
+            employee.Attendance();
+        }
+    }
+}
