a933a79b1336725a2cf172d3a3b8d70e82403ec1
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-10-26 12:45:03 +0530

    (Shubham)Add: Part time Employee and Wage

--- EmployeeWageProblem/EmployeeWage.cs
+++ EmployeeWageProblem/EmployeeWage.cs
@@ -3,7 +3,7 @@ namespace EmployeeWageProblem
 {
     public class EmployeeWage
     {
-        const int WAGE_PER_HR = 20,FULL_DAY_HR = 8,IS_PRESENT = 0,IS_FULL_TIME = 1;
+        const int WAGE_PER_HR = 20,FULL_DAY_HR = 8,HALF_DAY_HR = 4,IS_PRESENT = 0,IS_FULL_TIME = 1,IS_PART_TIME = 2;
         Random random = new Random();
         public void Attendance()
         {
@@ -17,13 +17,24 @@ namespace EmployeeWageProblem
         public void EmpWage()
         {
             int dailyEmpWage = 0,empHrs = 0;
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
             if (empCheck == IS_FULL_TIME)
+            {
                 empHrs = FULL_DAY_HR;
+                Console.WriteLine("Employee is present for full Day");
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                empHrs = HALF_DAY_HR;
+                Console.WriteLine("Employee is present for Half Day");
+            }
             else
+            {
                 empHrs = 0;
+                Console.WriteLine("Employee is Absent");
+            }
             dailyEmpWage = WAGE_PER_HR * empHrs;
-            Console.WriteLine("Daily Emp Wage" + dailyEmpWage);
+            Console.WriteLine("Daily Employee Wage is: "  + dailyEmpWage);
         }
     }
 }
