df628df56e4f5e2a8b1d078a555e95969a98db61
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-11-13 15:31:16 +0530

    (Shubham)Add: calculating monthly employee wage using constructor

--- EmployeeWageProblem/EmployeeWage.cs
+++ EmployeeWageProblem/EmployeeWage.cs
@@ -5,6 +5,16 @@ namespace EmployeeWageProblem
     {
         const int FULL_DAY_HR = 8, HALF_DAY_HR = 4, IS_PRESENT = 0, IS_FULL_TIME = 1, IS_PART_TIME = 2;
         Random random = new Random();
+        int WAGE_PER_HR,TOTAL_WORKING_DAYS,TOTAL_WORKING_HRS;
+        string COMPANY;
+        public EmployeeWage(string COMPANY, int WAGE_PER_HR, int TOTAL_WORKING_DAYS, int TOTAL_WORKING_HRS)
+        {
+            this.COMPANY = COMPANY;
+            this.WAGE_PER_HR = WAGE_PER_HR;
+            this.TOTAL_WORKING_DAYS = TOTAL_WORKING_DAYS;
+            this.TOTAL_WORKING_HRS = TOTAL_WORKING_HRS;
+        }
+    
         public void Attendance()
         {
 
@@ -14,10 +24,10 @@ namespace EmployeeWageProblem
             else
                 Console.WriteLine("Employee is Absent");
         }
-        public int EmpWage(int WAGE_PER_HR, int TOTAL_WORKING_DAYS, int TOTAL_WORKING_HRS)
+        public void EmpWage()
         {
             int totalEmpWage = 0, empHrs = 0;
-            for (int i = 0; i < TOTAL_WORKING_DAYS && empHrs< TOTAL_WORKING_HRS; i++)
+            for (int i = 0; i <TOTAL_WORKING_DAYS && empHrs< this.TOTAL_WORKING_HRS; i++)
             {
                 int empCheck = random.Next(0, 3);
                 switch (empCheck)
@@ -34,8 +44,8 @@ namespace EmployeeWageProblem
                 }
                 
             }
-            totalEmpWage = WAGE_PER_HR * empHrs;
-            return totalEmpWage;
+            totalEmpWage = this.WAGE_PER_HR * empHrs;
+            Console.WriteLine("Total monthly wage of employee for company "+COMPANY+" is "+totalEmpWage);
         }
     }
 }
--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -7,12 +7,10 @@ namespace EmployeeWageProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Problem statement");
-            EmployeeWage employee1 = new EmployeeWage();
-            int Wage1=employee1.EmpWage(80,20,100);
-            Console.WriteLine("Employee1 Monthly Wage is: " +Wage1);
-            EmployeeWage employee2 = new EmployeeWage();
-            int Wage2 = employee1.EmpWage(70, 25, 90);
-            Console.WriteLine("Employee2 Monthly Wage is: " + Wage2);
+            EmployeeWage HCL = new EmployeeWage("HCL",20,22,90);
+            HCL.EmpWage();
+            EmployeeWage JIO = new EmployeeWage("JIO",25,20,100);
+            JIO.EmpWage();
         }
     }
 }
